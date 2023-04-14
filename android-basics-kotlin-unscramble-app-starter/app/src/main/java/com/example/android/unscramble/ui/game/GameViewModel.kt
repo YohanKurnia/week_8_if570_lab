642af83bbc530f6682f25f120b3ed952127ee3e0
YohanKurnia <yohankw7@gmail.com>
2023-04-14 20:24:02 +0700

    Verify the ViewModel preserves data and Update game restart logic

--- android-basics-kotlin-unscramble-app-starter/app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ android-basics-kotlin-unscramble-app-starter/app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -18,7 +18,7 @@ class GameViewModel: ViewModel() {
             getNextWord()
         } else{
             _currentScrambledWord = String(tempWord)
-            ++currentWordCount
+            ++_currentWordCount
             wordsList.add(currentWord)
         }
     }
@@ -34,12 +34,14 @@ class GameViewModel: ViewModel() {
     private var _score = 0
     val score: Int
         get() = _score
-    private var currentWordCount = 0
+    private var _currentWordCount = 0
+    val currentWordCount: Int
+        get() = _currentWordCount
     private lateinit var _currentScrambledWord: String
     val currentScrambledWord: String
         get() = _currentScrambledWord
     fun nextWord(): Boolean{
-        return if (currentWordCount < MAX_NO_OF_WORDS){
+        return if (_currentWordCount < MAX_NO_OF_WORDS){
             getNextWord()
             true
         } else false
@@ -55,5 +57,12 @@ class GameViewModel: ViewModel() {
             return true
         }
         return false
+    }
+
+    fun reinitializeData(){
+        _score = 0
+        _currentWordCount = 0
+        wordsList.clear()
+        getNextWord()
     }
 }
