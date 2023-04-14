3e2c09fdeb19a1bc8315227f1301abcdbe6d873d
YohanKurnia <yohankw7@gmail.com>
2023-04-14 19:30:17 +0700

    Move data to the View Model and lifecycle of a ViewModel

--- android-basics-kotlin-unscramble-app-starter/app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ android-basics-kotlin-unscramble-app-starter/app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -1,6 +1,20 @@
 package com.example.android.unscramble.ui.game
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel: ViewModel() {
+    override fun onCleared() {
+        super.onCleared()
+        Log.d("GameFragment", "GameViewModel destroyed")
+    }
+
+    init {
+        Log.d("GameFragment", "GameViewModel created!")
+    }
+    private var score = 0
+    private var currentWordCount = 0
+    private var _currentScrambledWord = "test"
+    val currentScrambledWord: String
+        get() = _currentScrambledWord
 }
